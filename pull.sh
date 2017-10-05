fd2d3e29a24d14e9c6d9933285204262c6709f72
Alexander Novikov <anovikov@codeda.com>
2017-10-05 17:15:19 +0000

    Helpers

--- pull.sh
+++ pull.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
- 
-ssh-agent bash -c 'ssh-add /home/ubuntu/.ssh/id_rsa.git; git pull origin `git branch | grep \* | cut -d ' ' -f2-`'
+BRANCH=`git branch | grep \* | cut -d ' ' -f2-`
+
+ssh-agent bash -c "ssh-add /home/ubuntu/.ssh/id_rsa.git; git pull origin $BRANCH" 
--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
- 
-ssh-agent bash -c 'ssh-add /home/ubuntu/.ssh/id_rsa.git; git push origin `git branch | grep \* | cut -d ' ' -f2-`'
+BRANCH=`git branch | grep \* | cut -d ' ' -f2-`
+
+ssh-agent bash -c "ssh-add /home/ubuntu/.ssh/id_rsa.git; git push origin $BRANCH" 
