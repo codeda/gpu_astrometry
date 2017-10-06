7638ae8ac7230858f66b088d6dbbc773fb3423e9
Alexander Novikov <anovikov@codeda.com>
2017-10-06 05:28:32 +0000

    Load file from disk

--- testAstropy.py
+++ testAstropy.py
@@ -1,17 +1,24 @@
 
 
-
+from astropy.io import fits
 from astropy.stats import sigma_clipped_stats
 from photutils import datasets
-hdu = datasets.load_star_image()
-w=1059
-h=1059
+hdulist = fits.open("/home/ubuntu/IC.fit");
+hdulist.info()
+#exit();
+hdu=hdulist[0];
+
+w=hdu.header['NAXIS1']
+h=hdu.header['NAXIS2']
+print("w=", w, "h=", h)
+
+
 data = hdu.data[0:w, 0:h]
 mean, median, std = sigma_clipped_stats(data, sigma=3.0, iters=5)
 print((mean, median, std))
 
 from photutils import IRAFStarFinder
-daofind = IRAFStarFinder(fwhm=3.0, threshold=5.*std)
+daofind = IRAFStarFinder(fwhm=6.0, threshold=5.*std)
 sources = daofind(data - median)
 #print(sources)
 
