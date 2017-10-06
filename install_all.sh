38eb93d16959ccac88e7f88f5ecfca72c8ca17cb
angedonik <angedonik@gmail.com>
2017-10-06 13:32:03 +0300

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -30,12 +30,24 @@ if [[ $? -ne 0 ]]; then
     exit 1
 fi
 
+ssh-keygen -R $1
+
 scp ./init1.sh root@$1:/root
 invoke $1 root ./init1.sh
+
+if [ -z "$2" ]
+  then
+    branch='develop'
+  else
+    branch=$2
+fi
 
 scp ~/.ssh/id_rsa ubuntu@$1:/home/ubuntu/.ssh/id_rsa.git
 invoke $1 ubuntu "ssh-keyscan github.com >>/home/ubuntu/.ssh/known_hosts"
 invoke $1 ubuntu "git config --global user.email 'anovikov@codeda.com'"
 invoke $1 ubuntu "git config --global user.name 'Alexander Novikov'"
 invoke $1 ubuntu "ssh-agent bash -c 'ssh-add /home/ubuntu/.ssh/id_rsa.git; git clone git@github.com:codeda/gpu_astrometry.git'"
-invoke $1 ubuntu "cd gpu_astrometry && ./init2.sh"
+invoke $1 ubuntu "cd gpu_astrometry"
+invoke $1 ubuntu "git checkout "$branch
+invoke $1 ubuntu "./pull.sh"
+invoke $1 ubuntu "./init2.sh"
