dafc37251d3771b9cf445b9f7e4b74950050f8c1
novikovmaa <novikovmaa@gmail.com>
2017-10-06 13:34:55 +0300

    branch param

--- init2.sh
+++ init2.sh
@@ -12,6 +12,7 @@ sudo pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
 echo 'export PATH="$PATH:/home/ubuntu/.local/bin"' >>~/.bashrc
 
 sudo apt-get install -y python-dev
+sudo apt-get install -y python-tk
 sudo python -m pip install --upgrade pip
 sudo pip install cython --upgrade
 git clone git://github.com/numpy/numpy.git numpy
